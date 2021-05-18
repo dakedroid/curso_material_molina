de686d6adab6194dc076caaa862128d2adef81c8
Kevin David Molina Gomez <kevin.molina.go@gmail.com>
2021-05-18 00:08:21 -0500

    cambios

---
+++ app/src/main/java/mx/tecnm/voaxaca/curso_material_molina/ui/chats/ChatFragment.kt
@@ -0,0 +1,28 @@
+package mx.tecnm.voaxaca.curso_material_molina.ui.chats
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.tecnm.voaxaca.curso_material_molina.R
+
+class ChatFragment : Fragment() {
+
+    private lateinit var viewModel: ChatViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.chat_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/mx/tecnm/voaxaca/curso_material_molina/ui/chats/ChatViewModel.kt
@@ -0,0 +1,7 @@
+package mx.tecnm.voaxaca.curso_material_molina.ui.chats
+
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel : ViewModel() {
+
+}
