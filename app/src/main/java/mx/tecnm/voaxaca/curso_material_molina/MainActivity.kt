ef63d5b5681e8efc4bce0650202555cef9a2d065
Kevin David Molina Gomez <kevin.molina.go@gmail.com>
2021-05-19 15:23:18 -0500

    Se hizo la interfaz

--- app/src/main/java/mx/tecnm/voaxaca/curso_material_molina/MainActivity.kt
+++ app/src/main/java/mx/tecnm/voaxaca/curso_material_molina/MainActivity.kt
@@ -1,13 +1,9 @@
 package mx.tecnm.voaxaca.curso_material_molina
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 
-import mx.tecnm.voaxaca.curso_material_molina.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/mx/tecnm/voaxaca/curso_material_molina/ui/chats/ChatViewModel.kt
+++
@@ -1,7 +0,0 @@
-package mx.tecnm.voaxaca.curso_material_molina.ui.chats
-
-import androidx.lifecycle.ViewModel
-
-class ChatViewModel : ViewModel() {
-
-}
